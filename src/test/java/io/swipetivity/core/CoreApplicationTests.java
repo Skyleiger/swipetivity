0b3d49dddba6835a495cb9d13fc2d98956cacac9
Skyleiger <dominic.wienzek@gmail.com>
2023-12-14 00:48:26 +0100

    Replace backend with core

---
+++ src/test/java/io/swipetivity/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.swipetivity.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
